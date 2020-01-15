189a71294de11129e69dc958ba55f59c7bc01de7
Andrew Katz <andrewkatz00@gmail.com>
2020-01-14 16:23:47 -0800

    Add build dir for GH pages

---
+++ build/precache-manifest.35a9dc9ff29a07592ef26b02def88717.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e54c6bf7599ba249d199",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "f406443bbf4fc2157358",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "bf48e6fbf58647a79b63859df5725067",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e747cc0178ff1590af522a514a05830",
+    "url": "././manifest.json"
+  },
+  {
+    "revision": "08cb85f83b84db6bf6ecd2616534431b",
+    "url": "./assets/images/circle.png"
+  },
+  {
+    "revision": "6eb0c377e0ea2aac41ac8d8b229a8020",
+    "url": "./assets/images/icon-192px.png"
+  },
+  {
+    "revision": "dfb749fb41edc5c53626b50ec9d48776",
+    "url": "./assets/images/loader-bg.png"
+  },
+  {
+    "revision": "c97d238339edea4c33afd5a7f6a0d8ce",
+    "url": "./assets/images/icon-512px.png"
+  },
+  {
+    "revision": "78ea0555ef7648bf378481a5d778d344",
+    "url": "./assets/images/loader-bar.png"
+  },
+  {
+    "revision": "f8ba06c323a8fc17c0044df6df948a1f",
+    "url": "./assets/images/phaser-es6-webpack.jpg"
+  },
+  {
+    "revision": "bfd7f2fb9488329e9f3e8ca1993118ca",
+    "url": "./assets/images/mushroom2.png"
+  }
+];
